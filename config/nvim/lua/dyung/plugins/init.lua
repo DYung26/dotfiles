68a7a0b984bc75ad0db3e6931feef631c0df4022
DYung26 <theadstore1407@gmail.com>
2026-06-06 12:13:21 +0100

    feat(nvim): add plugin bootstrap entrypoint

---
+++ config/nvim/lua/dyung/plugins/init.lua
@@ -0,0 +1,12 @@
+require("dyung.plugins.lazy")
+
+-- require("dyung.plugins.legacy.packer")
+
+-- require("dyung.plugins.cmp")
+-- require("dyung.plugins.comment")
+-- require("dyung.plugins.copilot")
+-- require("dyung.plugins.git")
+-- require("dyung.plugins.indent")
+-- require("dyung.plugins.markdown")
+-- require("dyung.plugins.telescope")
+-- require("dyung.plugins.tree")

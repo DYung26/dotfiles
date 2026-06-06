578464bbf59353c64c52945a8fb6682a72ac00e8
DYung26 <theadstore1407@gmail.com>
2026-06-06 12:13:22 +0100

    feat(nvim): add NvimTree configuration module

---
+++ config/nvim/lua/dyung/plugins/tree.lua
@@ -0,0 +1,37 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwplugin = 1
+
+require('nvim-tree').setup({
+  renderer = {
+    indent_markers = {
+      enable = true,  -- show indent guides (lines)
+      inline_arrows = true, -- optional: small arrows instead of just lines
+    },
+  },
+  filters = {
+    dotfiles = false,
+    git_ignored = false,
+    custom = { ".*sync-conflict.*" },
+  },
+  git = {
+    enable = true,
+    ignore = false,
+  },
+  update_focused_file = {
+    enable = true,
+    update_root = false, -- Change to true if you want the root to change too
+  },
+})
+--[[{
+  actions = {
+    open_file = {
+      quit_on_open = true, -- optional
+    },
+  },
+  -- this helps prevent duplicate tree buffers
+  hijack_directories = {
+    enable = false,
+  },
+})
+]]
+
